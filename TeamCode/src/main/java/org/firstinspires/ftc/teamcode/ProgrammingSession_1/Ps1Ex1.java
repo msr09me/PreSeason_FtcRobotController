6fcd8266c32f8cf710d321e82099f6ee4ae6dce8
msr09me <shafiqwok@gmail.com>
2024-09-03 16:00:55 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingSession_1/Ps1Ex1.java
@@ -0,0 +1,24 @@
+/*
+
+Ps1Ex1: Create a variable to store the following data –
+
+                Left motor speed is 3.5
+
+ */
+
+
+package org.firstinspires.ftc.teamcode.ProgrammingSession_1;
+
+public class Ps1Ex1 {
+
+    public static void main(String args[])
+    {
+        // Start your code here
+
+        double speed = 3.5;
+
+        // End of your code
+
+        System.out.println("speed = " + speed);
+    }
+}
