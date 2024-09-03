53f00ca64967934eb1a5bf20ef3b0006c53d2b4b
msr09me <shafiqwok@gmail.com>
2024-09-03 19:01:42 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingSession_1/Ps1Ex2.java
@@ -0,0 +1,22 @@
+/*
+
+Ps1Ex2: A robot runs for 10 seconds. Create a variable to store the data
+
+ */
+
+
+package org.firstinspires.ftc.teamcode.ProgrammingSession_1;
+
+public class Ps1Ex2 {
+
+    public static void main(String[] args)
+    {
+        // Start your code here
+
+        double time = 10;
+
+        // End of your code
+
+        System.out.println("Time = " + time);
+    }
+}
