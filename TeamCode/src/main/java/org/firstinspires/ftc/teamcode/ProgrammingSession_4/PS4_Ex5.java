2e8633b0934ab9b827d7d9d3d7f543630e59b222
Amal Shehan Perera <62436735+amalshehan@users.noreply.github.com>
2024-09-11 20:08:08 -0400

    Create PS4_Ex5.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingSession_4/PS4_Ex5.java
@@ -0,0 +1,3 @@
+//Write a function to calculate the speed of an object when the distance and time in seconds is given
+//You can name your function appropriatley 
+//Call the function from the main 2 times with d=10, t=2 and d=100, t=3.2
